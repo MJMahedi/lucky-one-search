8c08a5f0d1bb4d501c536bf4e09f3d0946c6d978
MJMahedi <mdrokibulhasan5321@gmail.com>
2022-04-01 01:28:11 +0600

    header added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div>
+            <h2>Drop a Gear & Vanish !!</h2>
+            <h1>Choose Your Dream Bike for Today </h1>
+        </div>
+    );
+};
+
+export default Header;
