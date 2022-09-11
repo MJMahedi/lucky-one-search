ac678ced363d9ed1f661fd41d5cad2a05280f425
MJMahedi <mdrokibulhasan5321@gmail.com>
2022-09-11 23:45:43 +0600

    something added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css'
 const Header = () => {
     return (
         <div className='header'>
-            <h2>Drop a Gear & Disappear !!</h2>
+            <h2>Hey Drop a Gear & Disappear !! </h2>
             <h1>Choose Your Dream Bike for Today </h1>
         </div>
     );
